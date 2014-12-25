6f79a42c2942dfe3c31770fb8934a141847b3c3d
Steve Kondik <steve@cyngn.com>
2014-12-25 00:47:59 -0800

    Revert "Fixup armv8-a building, and make multiarch builds work"
     * This needs to be reconsideration, it doesn't work at all.
    
    This reverts commit b9c750dbf8d30c2dbe71871892eab7f495372548.
    
    Change-Id: I095994cc02104cf5de93e190feaf8cc12b669def

--- compat/strtod.c
+++ compat/strtod.c
@@ -42,8 +42,6 @@ static char *check_nan_suffix(char *s)
 #undef strtod
 double strtod(const char *, char **);
 
-double avpriv_strtod(const char *nptr, char **endptr);
-
 double avpriv_strtod(const char *nptr, char **endptr)
 {
     char *end;
